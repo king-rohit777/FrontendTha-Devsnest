d2d5c8be3445ee2e0ac04e246e107471ffab25e6
Rohit Jaiswal <62277327+king-rohit777@users.noreply.github.com>
2021-06-30 15:09:24 +0530

    Update call_apply_bind.js

--- DAY 9 - JS Question Set/call_apply_bind.js
+++ DAY 9 - JS Question Set/call_apply_bind.js
@@ -1,24 +1,29 @@
-//call , apply & bind
-
-//CALL
-const server1 = {
-    serverName: "Devsnest",
-    getInvite: function(a,b){
-      console.log(this);
-      console.log(a * b);
-    }
-  }
-  
-const server2 = {
-    serverName : "TheDeveloperCommunity"
+let name={
+  firstName:"Rohit",
+  lastName:"Jaiswal",
 }
-  
-server1.getInvite.call(server2,1,2); // ACTUALLY ITS => server1.getInvite.call() behind the scene.
 
-//APPLY
-server1.getInvite.apply(server2, [1,2]);
+let name2={
+  firstName:"Tanaya",
+  lastName:"Jaiswal",
+}
 
-//BIND used to return
-const ans = server1.getInvite.bind(server2, 1,2);
-//console.log(ans);
-ans();
+printFullName = function(hometown,state){
+  console.log(this.firstName+" "+this.lastName+" "+hometown
+  +" "+state);
+}
+
+//call
+//first parameter refers to this , rest are function parameters
+printFullName.call(name,"Jamshedpur","Jharkhand");
+printFullName.call(name2,"Jamshedpur too","Jharkhand too");
+
+//apply takes parameters as a list 
+printFullName.call(name,["Jamshedpur","Jharkhand"]);
+
+//bind creates a copy of the function, computes and stores the result which can be called
+//anytime and anywere in the function
+let getname=printFullName.bind(name2,"Jamshedpur","Jharkhand");
+//console.log(getname)
+getname();
+
