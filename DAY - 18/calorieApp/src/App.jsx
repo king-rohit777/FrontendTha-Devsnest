e425fc1ccd83cc94dfec1a12009560580ae806f8
Rohit Jaiswal <62277327+king-rohit777@users.noreply.github.com>
2021-07-14 20:30:41 +0530

    Add files via upload

---
+++ DAY - 18/calorieApp/src/App.jsx
@@ -0,0 +1,11 @@
+import CaloriePage from './components/CaloriePage'
+import Header from './components/Header'
+function App() {
+    return(
+        <div className="appCss">
+          <Header/>
+          <CaloriePage/>
+          </div>
+    );
+}
+export default App;
---
+++ DAY - 18/calorieApp/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Header.css';
+
+function nav(){
+return(
+<div className="header">
+    <h1 >Calorie Tracker List</h1>
+</div>
+)}
+
+export default nav;
