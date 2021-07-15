dc55c6e364a34016c871917a5c83774b4d3e04a0
Rohit Jaiswal <62277327+king-rohit777@users.noreply.github.com>
2021-07-15 17:30:05 +0530

    Add files via upload

---
+++ DAY - 18/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Chess from './chess';
+// import Card from './Card';
+import './chess.css';
+
+
+function App() {
+    return(
+        <div className="appStyle">
+          <h1>Chess Board </h1>
+          <div>
+          <Chess/>
+          </div>
+        </div>
+    );
+}
+export default App;
