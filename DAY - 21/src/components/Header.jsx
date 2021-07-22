636221dbfb512d90f8fdf5705a0d4a6eaee2a4e0
Rohit Jaiswal <62277327+king-rohit777@users.noreply.github.com>
2021-07-22 23:17:34 +0530

    Add files via upload

---
+++ DAY - 21/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+           <h1><strong>Todo-List</strong></h1>
+        </div>
+    )
+}
+
+export default Header;
