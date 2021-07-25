6d5782a4788ed4840acd08044de914459f72c382
Rohit Jaiswal <62277327+king-rohit777@users.noreply.github.com>
2021-07-25 15:46:13 +0530

    Update index.js

--- DAY - 07/index.js
+++ DAY - 07/index.js
@@ -30,7 +30,7 @@ console.log(student);
 // 3. Write a JavaScript program to get the length of a JavaScript object.  
 // Sample object : var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
 
-var student = {
+var student = {    
     name: "David Rayy",
     sclass: "VI",
     rollno: 12
